4df78a68288c184db8862b9ffdfbd0faf803495c
argadia38 <115652060+argadia38@users.noreply.github.com>
2022-11-23 22:40:03 +0700

    Update 2-dua.php

--- PHP/2-dua.php
+++ PHP/2-dua.php
@@ -13,8 +13,8 @@
     $Nama=array("Fanny","Fito","Yoga","Arga");
     $Kota=array("Magetan","Magetan","Madiun","Madiun");
     $TglLahir=array("09/03/2003","06/21/2003","08/16/2002","12/15/2003");
-    $NmBulan=array("","Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember");
-    $nmhari=array("","Senin","Selasa","Rabu","Kamis","Jumat","Sabtu","Minggu");
+    $NmBulan=array("September","Juni","Agustus","December");
+    $nmhari=array("Rabu","Sabtu","Jumat","Senin");
     ?>
     <table border="1" width="100%">
         <tr>
@@ -28,7 +28,7 @@
         <?php
         for ($i=0;$i<count($Nim);$i++)
         {
-            $Tgl=date("l, d F Y",strtotime($TglLahir[$i]));
+            $Tgl=date(" Y ",strtotime($TglLahir[$i]));
             $usia=date("Y")-date("Y",strtotime($TglLahir[$i]));
             ?>
             <tr>
@@ -36,7 +36,7 @@
                 <td><?php echo $Nim[$i]; ?></td>
                 <td><?php echo $Nama[$i]; ?></td>
                 <td><?php echo $Kota[$i]; ?></td>
-                <td><?php echo $Tgl; ?></td>
+                <td><?php echo $nmhari[$i],date(", d "),$NmBulan[$i],$Tgl;?></td>
                 <td><?php echo $usia." Tahun"; ?></td>
             </tr>
     <?php
